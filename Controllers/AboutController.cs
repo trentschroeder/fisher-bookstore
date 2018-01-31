3ec2bfcaa96f83630aac89fdaed02df198f3ede1
trentschroeder <trentschroede1@gmail.com>
2018-01-31 18:22:51 -0500

    Added routing and controllers except this is from the last lab so idk

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -11,7 +11,7 @@ namespace Fisher.Bookstore.Controllers
     {
         public IActionResult Index()
         {
-            return Content("hello world - about index");
+            return View();
         }
 
         public IActionResult History()
--- Controllers/OfferingsController.cs
+++ Controllers/OfferingsController.cs
@@ -15,7 +15,7 @@ public class OfferingsController : Controller
 
     public IActionResult Products()
     {
-        return Content("This is the products page.");
+        return View();
     }
 
     [Route("Services")]
@@ -23,7 +23,7 @@ public class OfferingsController : Controller
 
     public IActionResult Services()
     {
-        return Content("This is the services page.");
+        return View();
     }
 }
 
